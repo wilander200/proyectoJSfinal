814a7b3ac8055db85e779049562592376a3372b0
Wilander Velazco <wilandervelazco@gmail.com>
2021-12-08 22:33:26 -0300

    guardando avance 1

--- main.js
+++ main.js
@@ -6,37 +6,42 @@
 
 let contenedor = [];
 let botonCompra = document.querySelectorAll(".btnCarrito");
+let btnVaciarCarrito = document.getElementById("vaciarCarrito");
+document.addEventListener("DOMContentLoaded", readLocalStorage);
 
 for (let btn of botonCompra){
   btn.addEventListener("click", agregarCarrito);
 }
+btnVaciarCarrito.addEventListener("click", vaciarCarrito);
 
 function agregarCarrito(e) {
-  console.log(e.target);
+  e.preventDefault();
+  let hijo = e.target;
+  let padre = hijo.parentNode.parentNode.parentNode;
+  let biciRef = padre.querySelector("h5").textContent;
 
-  // programa para la lista
-
-  for (let i of bebidas) {
-    if (i.ref == parseInt(bebidaUser.value)) {
+ for (let i of bikes) {
+    if (i.ref == parseInt(biciRef)) {
       class datos {
-        constructor(nombre, precio, unidad, total) {
+        constructor(ref, nombre, precio) {
+          this.ref =ref;
           this.nombre = nombre;
           this.precio = precio;
-          this.unidad = unidad;
-          this.total = total;
         }
       }
       let producto = new datos(
+        i.ref,
         i.nombre,
-        i.precio,
-        parseInt(cantidadBebidas.value),
-        i.precio * parseInt(cantidadBebidas.value)
+        i.precio
       );
-      let lista = document.createElement("ul");
-      lista.innerHTML = `<li>${producto.nombre}</li>  <li>${producto.unidad}</li>  <li>${producto.precio} pesos</li>  <li>${producto.total} pesos</li>`;
-      let mensaje = document.getElementById("resultado");
-      mensaje.appendChild(lista);
+      let row = document.createElement("tr");
+      row.innerHTML = `<td>${producto.ref}</td> 
+      <td>${producto.nombre}</td>
+      <td>${producto.precio}</td>`;
+      let mensaje = document.getElementById("listaCarrito");
+      mensaje.appendChild(row);
       contenedor.push(producto);
+      guardarLocalStorage(contenedor);
     }
   }
   let totalPrecio = 0;
@@ -45,5 +50,42 @@ function agregarCarrito(e) {
     console.log(totalPrecio);
     let sumatoria = document.getElementById("sumatoria");
     sumatoria.innerHTML = `<h5>Total a pagar:  ${totalPrecio}</h5>`;
+    let totalIva = totalPrecio*1.19;
+    let totalFinal = document.getElementById("totalIva");
+    totalFinal.innerHTML = `<h5>Total con Iva:  ${totalIva}</h5>`;
   }
+} 
+
+function vaciarCarrito() {
+  while(producto.firstChild){
+    producto.removeChild(producto.firstChild);
+  }
+  return false;
+}
+
+function guardarLocalStorage(contenedor) {
+  let cont = traerLocalStorage();
+  contenedor.push(cont);
+  localStorage.setItem("contenedor" , JSON.stringify(contenedor)); 
+}
+
+function traerLocalStorage () {
+  let contenedorLS;
+  if (localStorage.getItem("contendor") === null) {
+    contenedorLS = [];
+  } else {
+    contenedor.LS = JSON.parse(localStorage.getItem("contenedor"));
+  }
+  return contenedorLS;
+}
+
+function readLocalStorage() {
+  let contenedorLS = traerLocalStorage();
+  contenedorLS.forEach(function(contenedor){
+    row.innerHTML = `<td>${producto.ref}</td> 
+    <td>${producto.nombre}</td>
+    <td>${producto.precio}</td>`;
+    let mensaje = document.getElementById("listaCarrito");
+    mensaje.appendChild(row);
+  });
 }
